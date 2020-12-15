f4d9b2ea199ca9af10caf9706f2e6f82befb4dfe
Polina <polinakrizz00@gmail.com>
2020-12-15 23:23:00 +0300

    Add buffers tests

---
+++ Google_tests/BufferTests.cpp
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+#include <Data/Buffer.hpp>
+#include <DataBuffers.hpp>
+
+TEST(BufferTest, StringSize) {
+    std::string str = "test";
+    Commons::Data::ConstBuffer buffer = Commons::Data::Buffer::string(str);
+    auto strData = static_cast<const char*>(buffer.data());
+    EXPECT_EQ(buffer.size(),str.length()+1);
+}
+
+TEST(BufferTest, StringData) {
+    std::string str = "test";
+    Commons::Data::ConstBuffer buffer = Commons::Data::Buffer::string(str);
+    auto strData = static_cast<const char*>(buffer.data());
+    EXPECT_STREQ(strData, str.c_str());
+}
+
+TEST(BufferTest, CharSize) {
+    const char* str = "test";
+    Commons::Data::ConstBuffer buffer = Commons::Data::Buffer::string(str);
+    auto strData = static_cast<const char*>(buffer.data());
+    EXPECT_EQ(buffer.size(),strlen(str)+1);
+}
+
+TEST(BufferTest, CharData) {
+    const char* str = "test";
+    Commons::Data::ConstBuffer buffer = Commons::Data::Buffer::string(str);
+    auto strData = static_cast<const char*>(buffer.data());
+    EXPECT_STREQ(strData, str);
+}
+
+
+
+
+
---
+++ Google_tests/MessageTests.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include <Data/Buffer.hpp>
+#include <DataBuffers.hpp>
+
+TEST(MessageTest, test1) {
+
+}
+
+TEST(MessageTest, test2) {
+
+}
